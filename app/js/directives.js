6e35cd88c4f6987925354a198d8e02b6643728ca
Arthur Debert <arthur@stimuli.com.br>
2012-11-09 11:58:29 -0200

    Use content editable for subtitle list

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,6 +7,13 @@ var directives = angular.module('myApp.directives', []);
 directives.directive('amaraEditableSubtitle', function() {
     return {
         link: function(scope, elm, attrs){
+            var el = angular.element(elm[0]);
+            var editableParagrah = $(elm[0]).children("p")[0];
+            editableParagrah.addEventListener('blur' , function() {
+                scope.$apply(function() {
+                    scope.subtitle.text = $(editableParagrah).text();
+                });
+            }, false);
             if (scope.mustScrollToBottom ){
                 var parent = elm[0].parentElement;
                 parent.scrollTop = parent.scrollHeight - parent.offsetHeight;

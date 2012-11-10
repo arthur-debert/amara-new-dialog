1b874c757bb47389115f71f6139cc853ecace346
Arthur Debert <arthur@stimuli.com.br>
2012-11-10 13:21:01 -0200

    Buggy as hell timeline directive

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.subtitleDataServices', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: SubtitleList});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.when('/subtitle', {templateUrl: 'partials/subtitle.html', controller: SubtitleList});
+    $routeProvider.when('/sync', {templateUrl: 'partials/sync.html', controller: SubtitleList});
+    $routeProvider.otherwise({redirectTo: '/subtitle'});
   }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -22,3 +22,61 @@ directives.directive('amaraEditableSubtitle', function() {
         }
     };
   });
+directives.directive('syncTimeline', function(){
+    var viewWidth = 600;
+    var zoomLevel = 1;
+    // normalized to zoom level 1
+    var millisecondsPerView = 6000;
+    var pixelsPerMillisecond = (viewWidth / millisecondsPerView) * zoomLevel;
+
+
+    var markerEveryMilliseconds = 500;
+    var startTime = 3200;
+
+    function timeToPixels(time){
+        return time * pixelsPerMillisecond;
+    }
+    function pixelsToTime(pixels){
+        return parseInt(time / pixelsPerMillisecond)
+    }
+    function nextMarkerTime(currentTime, markerEveryMilliseconds){
+        return Math.ceil(currentTime / markerEveryMilliseconds) * markerEveryMilliseconds;
+    }
+
+    function getMarkerTimes(startTime, markerEveryMilliseconds, millisecondsPerView){
+        var times = [];
+        var currentTime = startTime;
+        while(currentTime < startTime + millisecondsPerView){
+            var nextMarkerT = nextMarkerTime(currentTime, markerEveryMilliseconds);
+            if (nextMarkerT < startTime + millisecondsPerView){
+                times.push(nextMarkerT);
+            }
+            currentTime += markerEveryMilliseconds;
+        }
+        return times;
+    }
+
+    return {
+        link: function(scope, elm, attrs){
+            var timelineContainer = $("ul");
+            timelineContainer.css("width", viewWidth + "px");
+
+            var markerTimes = getMarkerTimes(startTime, markerEveryMilliseconds, millisecondsPerView);
+            console.log(markerTimes);
+            _.each(markerTimes, function(markerTime, i){
+                var ticker = $("<li>");
+                if (markerTime % 1000 == 0){
+                    ticker.text(i);
+                }else{
+                    ticker.text("|");
+                    ticker.addClass("timelineTicker");
+                }
+                // position
+                var xPos = timeToPixels(markerTime);
+                ticker.css("left", xPos);
+                timelineContainer.append(ticker);
+
+            });
+        }
+    }
+});

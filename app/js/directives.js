d2a5ef9de27af16b3c7ffb45e7fc07b8a03267cc
Arthur Debert <arthur@stimuli.com.br>
2012-11-10 21:47:27 -0200

    Timeline is draggable

--- app/js/directives.js
+++ app/js/directives.js
@@ -21,23 +21,31 @@ directives.directive('amaraEditableSubtitle', function() {
             }
         }
     };
-  });
+});
 directives.directive('syncTimeline', function(){
+    /**
+     * The time line is composed of two parts.
+     * The strip with the time markers (which is draggeable)
+     * and the timelineSubtitleList, containing the balloons with those subs
+     * @type {undefined}
+     */
+    var timelineEl = undefined;
     var viewWidth = 600;
     var zoomLevel = 1;
     // normalized to zoom level 1
     var millisecondsPerView = 6000;
     var pixelsPerMillisecond = (viewWidth / millisecondsPerView) * zoomLevel;
+    var currentMouseX, previousMouseX = undefined;
 
 
     var markerEveryMilliseconds = 500;
-    var startTime = 3200;
+    var currentTime = 0;
 
     function timeToPixels(time){
         return time * pixelsPerMillisecond;
     }
     function pixelsToTime(pixels){
-        return parseInt(time / pixelsPerMillisecond)
+        return parseInt(pixels / pixelsPerMillisecond)
     }
     function nextMarkerTime(currentTime, markerEveryMilliseconds){
         return Math.ceil(currentTime / markerEveryMilliseconds) * markerEveryMilliseconds;
@@ -56,27 +64,66 @@ directives.directive('syncTimeline', function(){
         return times;
     }
 
+    function redrawTimeline(timelineEl, currentTime){
+
+        $(timelineEl).children().remove();
+        var xOffset = timeToPixels(currentTime);
+        var markerTimes = getMarkerTimes(currentTime, markerEveryMilliseconds, millisecondsPerView);
+        _.each(markerTimes, function(markerTime, i){
+            var ticker = $("<li>");
+            if (markerTime % 1000 == 0){
+                ticker.text(parseInt(markerTime / 1000));
+            }else{
+                ticker.text("|");
+                ticker.addClass("timelineTicker");
+            }
+            // position
+            var xPos = timeToPixels(markerTime) - xOffset;
+            ticker.css("left", xPos);
+            timelineEl.append(ticker);
+
+        });
+    }
+
+    function registerMouse(e){
+        currentMouseX = e.pageX;
+        e.stopPropagation();
+    }
+    function onDragging(){
+        var xDelta = currentMouseX - previousMouseX;
+        if (xDelta == 0){
+            return;
+        }
+        var timeDelta = pixelsToTime(-xDelta);
+        var newTime = currentTime + timeDelta;
+        newTime = Math.max(0, newTime );
+        if (newTime != currentTime){
+            currentTime = newTime;
+            redrawTimeline(timelineEl, currentTime );
+            previousMouseX = currentMouseX;
+
+        }
+    }
+
     return {
         link: function(scope, elm, attrs){
-            var timelineContainer = $("ul");
-            timelineContainer.css("width", viewWidth + "px");
-            var xOffset = timeToPixels(startTime);
-            var markerTimes = getMarkerTimes(startTime, markerEveryMilliseconds, millisecondsPerView);
-            console.log(markerTimes);
-            _.each(markerTimes, function(markerTime, i){
-                var ticker = $("<li>");
-                if (markerTime % 1000 == 0){
-                    ticker.text(i);
-                }else{
-                    ticker.text("|");
-                    ticker.addClass("timelineTicker");
-                }
-                // position
-                var xPos = timeToPixels(markerTime) - xOffset;
-                ticker.css("left", xPos);
-                timelineContainer.append(ticker);
+            var dragTimeout = undefined;
+            timelineEl= $("ul");
+            timelineEl.css("width", viewWidth + "px");
+            redrawTimeline(timelineEl, currentTime);
+            function onStartTimelineDrag (e){
+                currentMouseX = previousMouseX = e.pageX;
+                dragTimeout = setInterval(onDragging, 40);
+                $(document).mousemove(registerMouse)
+            }
+            function onStopDragging(e){
+                $(document).unbind("onmousemove");
+                clearInterval(dragTimeout);
+            }
 
-            });
+            // atach drag and drop
+            timelineEl.mousedown( onStartTimelineDrag);
+            $(document).mouseup( onStopDragging);
         }
     }
 });
